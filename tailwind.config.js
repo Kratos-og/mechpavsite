f620d25fbd9686944cf86d701d331988e2e52c14
adawg <u@cnft.io>
2023-09-19 17:56:41 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'sans': ['rajdhani', 'sans-serif'],
+      'light': ['rajdhani-light', 'rajdhani', 'sans-serif'],
+      'medium': ['rajdhani-medium', 'rajdhani', 'sans-serif'],
+      'bold': ['rajdhani-bold', 'rajdhani', 'sans-serif']
+    },
+  },
+  plugins: [],
+}
+
