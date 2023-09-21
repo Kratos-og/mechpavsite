902079d4d398a64a9acbb03211c1f8d7b1e12e80
adawg <u@cnft.io>
2023-09-21 15:55:19 +0530

    camera curves smooth

--- components/Layout/Model/index.js
+++ components/Layout/Model/index.js
@@ -1,57 +1,85 @@
-
 import { scroll } from 'framer-motion';
 import Script from 'next/script';
 import { useEffect } from 'react';
+
 const Model = props => {
-    const snapPoints = [0, 33, 67, 100];
     let prevScroll = 0;
+
     useEffect(() => {
         scroll(onScroll, { axis: "y", source: document.getElementById("cont") })
     }, [])
 
     const onScroll = (y) => {
-        // let index = snapPoints.indexOf(y.toFixed(2) * 100);
-        // if (index > 0)
-        //     orbitModel(index);
         const val = y.toFixed(2) * 100;
-        let perc = (val / 33) * 100;
         let theta, phi, radius;
-        const currentVals = document.querySelector('#mech-model')?.cameraOrbit?.split(' ');
         if (val < 33) {
             if (prevScroll < val) {
                 //down
+                let perc = (val / 33) * 100;
                 //goal - -60deg 110deg 2m
-                theta = parseInt(currentVals[0]) - ((perc / 100) * (60 + parseInt(currentVals[0])));
-                phi = 105 + ((perc / 100) * 5);
-                radius = 6 - ((perc / 100) * 4)
-                orbitModel(theta, phi, radius);
+                theta = 0 - (perc * (60 / 100));
+                phi = 105 + (perc * (5 / 100));
+                radius = 6 - (perc * (4 / 100))
+                orbitModel(theta, phi, radius, val, perc);
             }
             else if (prevScroll > val) {
                 //up
-                theta = -60 + ((perc / 100) * 60);
-                phi = 110 - ((perc / 100) * 5);
-                radius = 2 + ((perc / 100) * 4)
-                console.log(parseInt(currentVals[0]), (perc / 100) * 60)
-                orbitModel(0, 105, 6);
+                //goal - 0deg 105deg 6m
+                let perc = (val / 33) * 100 - 100;
+                theta = -60 - (perc * (60 / 100));
+                phi = 110 + (perc * (5 / 100));
+                radius = 2 - (perc * (4 / 100))
+                orbitModel(theta, phi, radius, val, perc);
             }
         }
-
+        else if (val < 67 && val > 33) {
+            if (prevScroll < val) {
+                //down
+                //goal - -220deg 45deg 7m
+                let perc = ((val - 33) / 33) * 100;
+                theta = -60 - (perc * (160 / 100));
+                phi = 110 - (perc * (65 / 100));
+                radius = 2 + (perc * (5 / 100))
+                orbitModel(theta, phi, radius, val, perc);
+            }
+            else if (prevScroll > val) {
+                //up
+                //goal - -60deg 110deg 2m
+                let perc = ((val - 33) / 33) * 100 - 100;
+                theta = -216 - (perc * (160 / 100));
+                phi = 46 - (perc * (65 / 100));
+                radius = 7 + (perc * (5 / 100))
+                orbitModel(theta, phi, radius, val, perc);
+            }
+        }
+        else if (val <= 100 && val > 67) {
+            if (prevScroll < val) {
+                //down
+                //goal - -90deg 90deg 6m
+                let perc = ((val - 67) / 33) * 100;
+                theta = -220 + (perc * (130 / 100));
+                phi = 45 + (perc * (45 / 100));
+                radius = 7 - (perc * (1 / 100))
+                orbitModel(theta, phi, radius, val, perc);
+            }
+            else if (prevScroll > val) {
+                //up
+                //goal - -220deg 45deg 7m
+                let perc = ((val - 67) / 33) * 100 - 100;
+                theta = -90 + (perc * (130 / 100));
+                phi = 90 + (perc * (45 / 100));
+                radius = 6 - (perc * (1 / 100))
+                orbitModel(theta, phi, radius, val, perc);
+            }
+        }
         prevScroll = val;
     }
 
-    const orbitModel = (theta, phi, radius) => {
+    const orbitModel = (theta, phi, radius, val, perc) => {
         const modelViewer = document.querySelector('#mech-model');
-        const orbitCycle = [
-            "0deg 105deg 6m",
-            '-60deg 110deg 2m', // 0m 1m 0m
-            '140deg 45deg 7m', // "1m 1.5m 0m"
-            "-90deg 90deg 6m", //"1m 1.5m 0m" 
-        ];
-        //console.log(theta, phi, radius)
-        //if (modelViewer.cameraOrbit !== orbitCycle[index]) {
-        modelViewer.cameraOrbit = `${theta}deg ${phi}deg ${radius}m`;
-        //}
+        console.log(theta.toFixed(2), phi.toFixed(2), radius.toFixed(2), val.toFixed(2), perc.toFixed(2))
 
+        modelViewer.cameraOrbit = `${theta}deg ${phi}deg ${radius}m`;
     }
     return (
         <div className='w-full h-full'>
