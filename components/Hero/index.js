dfc824f9737199654e64202c61f927fc9e797950
Bharath <bharathj0410@gmail.com>
2023-09-21 19:16:23 +0530

    done with page one

---
+++ components/Hero/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "./Button";
+import Mouse from "./Mouse";
+
+export default function index() {
+  return (
+    <div className="flex items-center h-screen w-full">
+      <div className="w-full">
+        <div className="text-[18rem] font-extrabold leading-none flex justify-center">
+          <p>MECHPAVS</p>
+        </div>
+        <div className="tracking-[0.2rem] text-sm">
+          <div>
+            CARDANO&apos;S BIGGEST NFT MINT IS HAPPENING RIGHT HERE ON <span className="font-bold">OCTOBER 19<sup>th</sup></span>
+          </div>
+          <div className=" text-xs pt-2 tracking-[0.1rem]">5 CLASSES, 5 PARTS,5 SKINS AND OVER 1 MILLION COMBINATIONS COLLECT 
+          AND BUILD INTERCHANGEABLE MECH PARTS TO BUILD A FULLY USABLE AVATAR.</div>
+        </div>
+        <div className="flex items-center justify-between w-full">
+          <div className="pt-20">
+            <Button text={"join with us"} />
+          </div>
+          <div className="w-16 translate-y-14">
+            <Mouse />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,29 +1,10 @@
-import React from "react";
-import Button from "../Layout/Button";
-import Mouse from "../Layout/Mouse";
+import React from 'react'
+import Hero from "../Hero"
 
 export default function index() {
   return (
-    <div className="h-screen flex justify-center items-center">
-      <div>
-        <div className="text-[15rem] font-medium leading-none">
-          <p>MECHPAVS</p>
-        </div>
-        <div className="tracking-[0.3rem] text-sm">
-          <div>
-            WE ARE <span className="font-bold">FRONTRIBE</span>
-          </div>
-          <div>CAREER MANAGEMENT SERVICE FOR FRONTEND DEVELOPERS</div>
-        </div>
-        <div className="flex items-center justify-between">
-          <div className="pt-20 w-[12rem]">
-            <Button text={"join as developer"} />
-          </div>
-          <div className="w-16 translate-y-11">
-            <Mouse />
-          </div>
-        </div>
-      </div>
+    <div className='bg-black px-[10%] w-screen overflow-hidden'>
+      <Hero/>
     </div>
-  );
+  )
 }
