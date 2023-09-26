c279acf14c8c30e358fd24ef6712b7e6027ba22e
adawg <u@cnft.io>
2023-09-27 00:28:33 +0530

    fix

--- pages/Lore/Page1/index.js
+++ pages/Lore/Page1/index.js
@@ -1,24 +1,22 @@
-import React,{useState} from "react";
-import { motion,useMotionValueEvent } from "framer-motion";
-import Image from "next/image";
-import space from "../../../public/assets/img/space.png";
+import React, { useState } from "react";
+import { motion, useMotionValueEvent } from "framer-motion";
 
 export default function Index(props) {
-  const [scroll,setScroll] = useState();
+  const [scroll, setScroll] = useState(0);
+
   useMotionValueEvent(props.progress, "change", (latest) => {
     setScroll(latest.toFixed(2))
   });
-  console.log(scroll)
+  console.log(1 + scroll * 5);
+
   return (
     <div className="w-full h-screen ">
-
       <motion.div
-      initial={{scale:1}}
-      animate={{scale: 1 + scroll * 5}}
-      className="fixed ">
-        <Image src={space} alt="space" className="w-[200vh] -translate-y-3 translate-x-10" />
+        initial={{ scale: 1 }}
+        animate={{ scale: 1 + (scroll) * 5 }}
+        className="fixed ">
+        <img src={'/assets/img/space.png'} alt="space" className="w-screen object-cover" />
       </motion.div>
-
     </div>
   );
 }
