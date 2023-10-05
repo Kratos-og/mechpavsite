a10eaba90ef55b050fa5803a8150bb858b7ec322
adawg <u@cnft.io>
2023-10-05 20:40:08 +0530

    fix

--- components/Home/Hero/Pointers/Pointer1/index.js
+++ components/Home/Hero/Pointers/Pointer1/index.js
@@ -2,16 +2,16 @@ import { motion } from "framer-motion";
 
 const Pointer1 = props => {
     return (
-        <div className="-ml-60 mt-3">
+        <div className="-ml-52 -mt-7">
             <motion.div
                 initial={{ opacity: 0 }}
                 animate={{
                     opacity: [0, 0.5, 0, 1, 0, 1],
                     transition: { delay: 1.5, duration: 1, ease: "easeOut" },
                 }}
-                className="text-[10px] text-white"
+                className="text-[10px] -ml-7 text-white"
             >
-                <span>Apollo joint connectors. <br />Version 2</span>
+                 <span>Apollo joint connectors. <br />Version 2</span>
             </motion.div>
             <motion.svg
                 version="1.1"
@@ -19,16 +19,14 @@ const Pointer1 = props => {
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
                 y="0px"
-                viewBox="0 0 602.8 232.1"
+                viewBox="0 0 602.8 222.1"
                 fill="#ffffff"
-                className="w-[15rem] h-full"
+                className="w-[15rem]"
             >
                 <motion.circle
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1, transition: { delay: 0.5, duration: 1 } }}
-                    className="st0"
-                    cx="570"
-                    cy="176.9"
+                    cx="493.5" cy="169.5"
                     r="7"
                 />
                 <motion.path
@@ -37,7 +35,7 @@ const Pointer1 = props => {
                         pathLength: 1,
                         transition: { delay: 0.5, duration: 1, ease: "easeInOut" },
                     }}
-                    d="M570,176.9V208H122c-38.7,0-70-31.3-70-70V26"
+                    d="M493.5,169.5H99.4c-38.7,0-70-31.3-70-70V64.2"
                     strokeWidth={4}
                     stroke={"white"}
                     fill={"transparent"}
