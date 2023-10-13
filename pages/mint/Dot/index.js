f48285394165b7eb7a42517a5be810450a716bf2
adawg <u@cnft.io>
2023-10-13 13:23:30 +0530

    fixes

--- pages/mint/Dot/index.js
+++ pages/mint/Dot/index.js
@@ -3,14 +3,14 @@ import { motion } from "framer-motion";
 export default function index(props) {
   return (
     <div className="flex gap-5 items-center">
-      <div className="relative">
-        <div className="bg-[#1af8c4] rounded-full p-[0.3rem]"></div>
-        <motion.div 
-        initial={{scale:0}}
-        animate={{scale:2.5,transition:{repeat:Infinity,duration:0.8,ease:"easeInOut"}}}
-        className="bg-[#1af8c4]/60 rounded-full absolute p-[0.3rem] bottom-[0.01rem]"></motion.div>
+      <div className="relative flex items-center">
+        <div className="bg-[#1af8c4] rounded-full p-[0.2rem]"></div>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 2.5, transition: { repeat: Infinity, duration: 1.5, ease: "easeInOut" } }}
+          className="bg-[#1af8c4]/60 rounded-full absolute p-[0.2rem]"></motion.div>
       </div>
-      <p>{props.children}</p>
+      <div>{props.children}</div>
     </div>
   );
 }
--- pages/mint/Utility/index.js
+++ pages/mint/Utility/index.js
@@ -5,14 +5,17 @@ import { motion } from 'framer-motion'
 export default function index(props) {
   return (
     <div>
-      <p className="flex items-center gap-5"><p className="text-bold text-[#1af8c4]">
-        <motion.div 
-        initial={{rotate:0}}
-        animate={{rotate:90,transition:{repeat:Infinity,duration:1,ease:'easeInOut',repeatDelay:0.5}}}
-        >
-        <FaPlus/>
-        </motion.div>
-        </p>{props.children}</p>
+      <div className="flex items-center gap-5">
+        <div className="text-bold text-[#1af8c4]">
+          <motion.div
+            initial={{ rotate: 0 }}
+            animate={{ rotate: 90, transition: { repeat: Infinity, duration: 1, ease: 'easeInOut', repeatDelay: 0.5 } }}
+          >
+            <FaPlus />
+          </motion.div>
+        </div>
+        {props.children}
+      </div>
     </div>
   )
 }
