fa709ac6fa5ff9c21530d9e78693b6da2fb1bed1
adawg <u@cnft.io>
2023-10-19 13:18:03 +0530

    fixes

--- components/Minting/Text/index.js
+++ components/Minting/Text/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { useTypewriter } from "react-simple-typewriter";
 
-export default function Index({text,speed,textHandler}) {
-  const [typewriter_text] = useTypewriter({
+export default function Index({ text, speed, index, onDone }) {
+  const [typewriter_text, helper] = useTypewriter({
     words: [text],
     loop: 1,
-    onLoopDone: () => {
-        textHandler(true)
-    },
-    typeSpeed:speed
+    typeSpeed: speed
   });
+  const { isType, isDelete, isDelay, isDone } = helper;
+  if (isDone) {
+    onDone(index + 1)
+  }
   return (
     <div>
       <p>{typewriter_text}</p>
