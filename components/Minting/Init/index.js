235da449c1e0d362c09145532d32e4429d48faea
adawg <u@cnft.io>
2023-10-19 16:30:34 +0530

    wallet changes

--- components/Minting/Init/index.js
+++ components/Minting/Init/index.js
@@ -7,15 +7,6 @@ import Crate from "../Crate";
 const Init = props => {
     const [walletDetails, setWalletDetails] = useState(false);
     const [activeIndex, setActiveIndex] = useState(0)
-    const [typing, setTyping] = useState({
-        text1: false,
-        text2: false,
-        text3: false,
-        text4: false,
-        text5: false,
-        text6: false,
-    })
-
 
     return (
         <div className="flex h-[88vh] pt-20 px-10 justify-evenly gap-10">
@@ -29,69 +20,71 @@ const Init = props => {
                         loop={1}
                     />}
                 </p>
-                <div className="px-5 py-2">
-                    <p className="pt-5 text-gray-500">
-                        {activeIndex >= 0 ? <Text index={0} onDone={setActiveIndex} text={"> < STARTING . . . >"} speed={50}/> : null}
-                    </p>
-                    {activeIndex >= 1 && <Text index={1} onDone={setActiveIndex} text={"> WELCOME TO MECH PAV MINTING . . ."} speed={50} />}
-                    <p className="pt-5 text-gray-500">
-                        {activeIndex >= 2 && <Text index={2} onDone={setActiveIndex} text={"> < INTRO >"} speed={50} />}</p>
-                    {activeIndex >= 3 && <Text index={3} onDone={setActiveIndex} text={"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore itaque a aut fugiat labore veritatis nesciunt tempore aperiam voluptatum magnam."} speed={30} />}
-                    <p className="pt-5 text-gray-500">
-                        {activeIndex >= 4 && <Text index={4} onDone={setActiveIndex} text={"> < INFO >"} speed={50} />}</p>
-                    {activeIndex >= 5 && <Text index={5} onDone={setActiveIndex} text={"> Waiting for User Input . . . "} speed={50} />}
-                    {walletDetails && (
-                        <div>
-                            <p className="pb-5 text-[#14fecdff]">
-                                <Typewriter
-                                    words={["> Wallet connection successful . . . "]}
-                                    cursorStyle="_"
-                                    cursorColor="#14fecdff"
-                                    loop={1}
-                                    typeSpeed={1}
-                                />
-                            </p>
-
-                            <p className="text-gray-500 pb-2">
-                                <Typewriter
-                                    words={["> < DONE >"]}
-                                    cursorStyle="_"
-                                    cursorColor="#14fecdff"
-                                    loop={1}
-                                />
-                            </p>
-                            <p className=" text-[#14fecdff]">
-                                <Typewriter
-                                    words={["> ALL SET FOR MECH MINTING . . . "]}
-                                    cursorStyle="_"
-                                    cursorColor="#14fecdff"
-                                    loop={1}
-                                    typeSpeed={1}
-                                />
-                            </p>
-                            <div className="w-full px-3 border-2 border-[#423F3E] mt-3">
-                                <button className="border-2 border-[#423F3E] rounded-full w-full mt-3 py-2 uppercase hover:bg-[#14fecdff] hover:text-black ease-in-out duration-300 text-sm ">
-                                    NEXT
-                                </button>
-                                <p className="text-[0.65rem] py-3">
+                <div className="px-2 py-3 h-[90%]">
+                    <div className="px-3 py-2 overflow-auto custom-scroll h-full">
+                        <p className="pt-5 text-gray-500">
+                            {activeIndex >= 0 ? <Text index={0} onDone={setActiveIndex} text={"> < STARTING . . . >"} speed={50} /> : null}
+                        </p>
+                        {activeIndex >= 1 && <Text index={1} onDone={setActiveIndex} text={"> WELCOME TO MECH PAV MINTING . . ."} speed={50} />}
+                        <p className="pt-5 text-gray-500">
+                            {activeIndex >= 2 && <Text index={2} onDone={setActiveIndex} text={"> < INTRO >"} speed={50} />}</p>
+                        {activeIndex >= 3 && <Text index={3} onDone={setActiveIndex} text={"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolore itaque a aut fugiat labore veritatis nesciunt tempore aperiam voluptatum magnam."} speed={30} />}
+                        <p className="pt-5 text-gray-500">
+                            {activeIndex >= 4 && <Text index={4} onDone={setActiveIndex} text={"> < INFO >"} speed={50} />}</p>
+                        {activeIndex >= 5 && <Text index={5} onDone={setActiveIndex} text={"> Waiting for User Input . . . "} speed={50} />}
+                        {walletDetails && (
+                            <div>
+                                <p className="pb-5 text-[#14fecdff]">
                                     <Typewriter
-                                        words={["> ERROR: NULL"]}
+                                        words={["> Wallet connection successful . . . "]}
                                         cursorStyle="_"
                                         cursorColor="#14fecdff"
                                         loop={1}
                                         typeSpeed={1}
-                                        cursor
                                     />
                                 </p>
+
+                                <p className="text-gray-500 pb-2">
+                                    <Typewriter
+                                        words={["> < DONE >"]}
+                                        cursorStyle="_"
+                                        cursorColor="#14fecdff"
+                                        loop={1}
+                                    />
+                                </p>
+                                <p className=" text-[#14fecdff]">
+                                    <Typewriter
+                                        words={["> ALL SET FOR MECH MINTING . . . "]}
+                                        cursorStyle="_"
+                                        cursorColor="#14fecdff"
+                                        loop={1}
+                                        typeSpeed={1}
+                                    />
+                                </p>
+                                <div className="w-full px-3 border-2 border-[#423F3E] mt-3">
+                                    <button className="border-2 border-[#423F3E] rounded-full w-full mt-3 py-2 uppercase hover:bg-[#14fecdff] hover:text-black ease-in-out duration-300 text-sm ">
+                                        NEXT
+                                    </button>
+                                    <p className="text-[0.65rem] py-3">
+                                        <Typewriter
+                                            words={["> ERROR: NULL"]}
+                                            cursorStyle="_"
+                                            cursorColor="#14fecdff"
+                                            loop={1}
+                                            typeSpeed={1}
+                                            cursor
+                                        />
+                                    </p>
+                                </div>
                             </div>
+                        )}
+                        <div>
+                            {!walletDetails && (
+                                <Wallet onConnect={setWalletDetails} />
+                            )
+                            }
                         </div>
-                    )}
-                </div>
-                <div>
-                    {!walletDetails && (
-                        <Wallet onConnect={setWalletDetails} />
-                    )
-                    }
+                    </div>
                 </div>
             </div>
             <div className="w-3/4 border-2 border-[#423F3E] relative overflow-hidden">
