c76e7badd11dfca1a3b5618bd878a50dc176419d
adawg <u@cnft.io>
2023-10-28 00:26:40 +0530

    changes

--- components/Unboxing/Wallet/index.js
+++ components/Unboxing/Wallet/index.js
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { TransactionUnspentOutput } from "@emurgo/cardano-serialization-lib-browser";
 import { Lucid, C } from "lucid-cardano";
+import SpinnerSm from "@/components/UI/SpinnerSm";
 
 const Wallet = props => {
     const [walletError, setWalletError] = useState("");
     const [loading, setLoading] = useState(false);
-    const [wallet, setWallet] = useState(null);
+    const [wallet, onWalletSelect] = useState(null);
 
-    const onWalletSelect = async (wallet) => {
+    const onContinue = async () => {
         try {
             if (!window.cardano) {
                 setLoading(false);
@@ -22,8 +23,17 @@ const Wallet = props => {
             setLoading(true);
             const walletInstance = await window.cardano[wallet.toLowerCase()]?.enable();
             const utxos = await walletInstance.getUtxos();
-            const validTokens = getValidTokens(utxos, ["29cd56a575e5c1dc3a03ff687f0651d728b28fee52a932b508242b4a"]);
-            console.log(validTokens)
+            const usedAddr = await walletInstance.getUsedAddresses();
+            const unUsedAddr = await walletInstance.getUnusedAddresses();
+            setLoading(false);
+            let resUtxos = utxos;
+            if (wallet == 'eternl') {
+                let collateral = await walletInstance.getCollateral();
+                resUtxos = [...utxos, ...collateral]
+            }
+            const validTokens = getValidTokens(resUtxos, ["29cd56a575e5c1dc3a03ff687f0651d728b28fee52a932b508242b4a"]);
+            props.onConnect({ name: wallet, address: [...usedAddr, ...unUsedAddr], utxos: resUtxos });
+            props.setAssets(validTokens);
             setLoading(false);
 
         }
@@ -88,45 +98,57 @@ const Wallet = props => {
 
     return (
         <div className="">
-            <div className="font-bold uppercase mb-4">Connect wallet to continue</div>
-            <div className='py-4 flex flex-col gap-4 h-[350px] overflow-y-auto overflow-x-hidden custom-scroll pr-3'>
-                <div className='flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2' onClick={() => onWalletSelect('lace')}>
-                    <img src='/assets/images/wallets/lace.svg' className='w-4' />
-                    <div>Lace Wallet</div>
+
+            {loading ?
+                <div className="flex justify-center">
+                    <SpinnerSm />
                 </div>
-                <div className='flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2' onClick={() => onWalletSelect('eternl')}>
-                    <img src='/assets/images/wallets/eternl.png' className='w-4' />
-                    <div>Eternl Wallet</div>
-                </div>
-                <div className='flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2' onClick={() => onWalletSelect('nami')}>
-                    <img src='/assets/images/wallets/nami.svg' className='w-4' />
-                    <div>Nami Wallet</div>
-                </div>
-                <div className='flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2' onClick={() => onWalletSelect('flint')}>
-                    <img src='/assets/images/wallets/flint.svg' className='w-4' />
-                    <div>Flint Wallet</div>
-                </div>
-                <div className='flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2' onClick={() => onWalletSelect('gero')}>
-                    <img src='/assets/images/wallets/gero.ico' className='w-4' />
-                    <div>Gero Wallet</div>
-                </div>
-                <div className='flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2' onClick={() => onWalletSelect('typhoncip30')}>
-                    <img src='/assets/images/wallets/typhon.png' className='w-4' />
-                    <div>Typhon Wallet</div>
-                </div>
-                <div className='flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2' onClick={() => onWalletSelect('vespr')}>
-                    <img src='/assets/images/wallets/vespr.jpg' className='w-4' />
-                    <div>Vespr Wallet</div>
-                </div>
-                <div className='flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2' onClick={() => onWalletSelect('begin')}>
-                    <img src='/assets/images/wallets/begin.png' className='w-4' />
-                    <div>Begin Wallet</div>
-                </div>
-                <div className='flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2' onClick={() => onWalletSelect('nufi')}>
-                    <img src='/assets/images/wallets/nufi.png' className='w-4' />
-                    <div>Nufi Wallet</div>
-                </div>
-            </div>
+                :
+                <>
+                    <div className="font-bold uppercase mb-4">Connect wallet to continue</div>
+                    <div className='py-4 flex flex-col gap-4 h-[350px] overflow-y-auto overflow-x-hidden custom-scroll pr-3'>
+                        <div className={`flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2 ${wallet == 'lace' ? 'bg-white text-black' : ''}`} onClick={() => onWalletSelect('lace')}>
+                            <img src='/assets/images/wallets/lace.svg' className='w-4' />
+                            <div>Lace Wallet</div>
+                        </div>
+                        <div className={`flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2 ${wallet == 'eternl' ? 'bg-white text-black' : ''}`} onClick={() => onWalletSelect('eternl')}>
+                            <img src='/assets/images/wallets/eternl.png' className='w-4' />
+                            <div>Eternl Wallet</div>
+                        </div>
+                        <div className={`flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2 ${wallet == 'nami' ? 'bg-white text-black' : ''}`} onClick={() => onWalletSelect('nami')}>
+                            <img src='/assets/images/wallets/nami.svg' className='w-4' />
+                            <div>Nami Wallet</div>
+                        </div>
+                        <div className={`flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2 ${wallet == 'flint' ? 'bg-white text-black' : ''}`} onClick={() => onWalletSelect('flint')}>
+                            <img src='/assets/images/wallets/flint.svg' className='w-4' />
+                            <div>Flint Wallet</div>
+                        </div>
+                        <div className={`flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2 ${wallet == 'gero' ? 'bg-white text-black' : ''}`} onClick={() => onWalletSelect('gero')}>
+                            <img src='/assets/images/wallets/gero.ico' className='w-4' />
+                            <div>Gero Wallet</div>
+                        </div>
+                        <div className={`flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2 ${wallet == 'typhon' ? 'bg-white text-black' : ''}`} onClick={() => onWalletSelect('typhoncip30')}>
+                            <img src='/assets/images/wallets/typhon.png' className='w-4' />
+                            <div>Typhon Wallet</div>
+                        </div>
+                        <div className={`flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2 ${wallet == 'vespr' ? 'bg-white text-black' : ''}`} onClick={() => onWalletSelect('vespr')}>
+                            <img src='/assets/images/wallets/vespr.jpg' className='w-4' />
+                            <div>Vespr Wallet</div>
+                        </div>
+                        <div className={`flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2 ${wallet == 'begin' ? 'bg-white text-black' : ''}`} onClick={() => onWalletSelect('begin')}>
+                            <img src='/assets/images/wallets/begin.png' className='w-4' />
+                            <div>Begin Wallet</div>
+                        </div>
+                        <div className={`flex items-center gap-2 hover:bg-white hover:text-black px-3 cursor-pointer transition-all will-change-transform py-2 ${wallet == 'nufi' ? 'bg-white text-black' : ''}`} onClick={() => onWalletSelect('nufi')}>
+                            <img src='/assets/images/wallets/nufi.png' className='w-4' />
+                            <div>Nufi Wallet</div>
+                        </div>
+                    </div>
+                </>
+            }
+            <button disabled={!wallet} onClick={onContinue} className="absolute disabled:border-gray-600 disabled:hover:bg-black disabled:hover:text-gray-600 disabled:hover:cursor-not-allowed disabled:text-gray-600 font-bold bottom-5 right-5 px-8 py-3 border cursor-pointer hover:bg-white hover:text-black transition-all">
+                Continue <span className="ml-2">&rarr;</span>
+            </button>
         </div>
     )
 }
--- pages/unbox/index.js
+++ pages/unbox/index.js
@@ -1,17 +1,85 @@
-import BgAnim from "@/components/Unboxing/BgAnim";
+import Crate from "@/components/Unboxing/Crate";
 import Wallet from "@/components/Unboxing/Wallet";
+import { useState } from "react";
+import axios from '@/components/Common/axios';
 
 const Unbox = props => {
+    const [assets, setAssets] = useState([]);
+    const [selected, setSelected] = useState([]);
+    const [walletDetails, setWalletDetails] = useState(null);
 
+    const onCrateSelect = (crateId) => {
+        setSelected([...selected, crateId]);
+    }
+
+    const onMintInitiate = async () => {
+        try {
+            let data = (await axios.post(`/buy/653a351e79b53e832360b35b`,
+                {
+                    utxoStrings: walletDetails.utxos,
+                    addresses: walletDetails?.address,
+                    number: selected.length,
+                    options: {
+                        cratesToBurn: selected
+                    }
+                }
+            )).data;
+            const sig = await signTransaction(data?.transactionBodyHex);
+            if (sig) {
+                let submit = await submitTransaction(sig, data?.transactionBodyHex);
+
+                if (submit) {
+
+                }
+            }
+        }
+        catch (error) {
+            console.log(error)
+            return;
+        }
+    }
+
+    const signTransaction = async (txHex) => {
+        try {
+            const walletInstance = await window.cardano[walletDetails?.name?.toLowerCase()]?.enable();
+            const sig = await walletInstance.signTx(txHex, true);
+            return sig;
+        }
+        catch (error) {
+            console.log(error)
+            return;
+        }
+    };
+
+    const submitTransaction = async (witnessSetHex, txHex) => {
+        try {
+            let data = (await axios.post(`/submit`,
+                {
+                    witnessSetHex: witnessSetHex,
+                    transactionHex: txHex,
+                }
+            )).data;
+            return data;
+        }
+        catch (error) {
+        }
+    };
 
     return (
         <div className="w-full min-h-screen p-20">
-            <div className="w-full relative border flex items-center justify-center border-gray-700 h-[600px] rounded-2xl">
-                <div className="cursor-pointer absolute w-[95%] left-7 font-bold -top-5 p-5 border rounded-xl bg-white text-black uppercase">Mech Crates Unboxing</div>
-                <Wallet />
-                <div className="absolute font-bold bottom-5 right-5 px-8 py-3 border cursor-pointer hover:bg-white hover:text-black transition-all">
-                    Continue <span className="ml-2">&rarr;</span>
-                </div>
+            <div className="w-full relative border flex items-center justify-center border-gray-700 h-[600px]">
+                <div className="cursor-pointer absolute w-[95%] left-7 font-bold -top-6 p-5 border bg-white text-black uppercase">Mech Crates Unboxing</div>
+                {
+                    !assets.length ? <Wallet setAssets={setAssets} onConnect={setWalletDetails} /> :
+                        <div className="w-full h-full">
+                            <div className="flex flex-wrap gap-5 w-full h-full overflow-y-auto custom-scroll mt-5 items-center">
+                                {assets.map((item, i) => <Crate item={item} onClick={() => onCrateSelect(item)} />)}
+                            </div>
+                            <button onClick={onMintInitiate} disabled={!selected.length} className="absolute disabled:border-gray-600 disabled:hover:bg-black disabled:hover:text-gray-600 disabled:hover:cursor-not-allowed disabled:text-gray-600 font-bold bottom-5 right-5 px-8 py-3 border cursor-pointer hover:bg-white hover:text-black transition-all">
+                                Continue <span className="ml-2">&rarr;</span>
+                            </button>
+                        </div>
+                }
             </div>
 
         </div>
