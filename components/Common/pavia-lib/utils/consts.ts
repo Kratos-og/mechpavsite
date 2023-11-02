f78887f4d231e9a9343658502731e7a6639dd456
Philip <philipmarsh152@gmail.com>
2023-11-02 11:12:48 +0000

    point dev site to prod to check for any config issues

--- components/Common/pavia-lib/utils/consts.ts
+++ components/Common/pavia-lib/utils/consts.ts
@@ -1,2 +1,2 @@
-// export const baseAPIUrl = "https://api.pavia.io/playground";
-export const baseAPIUrl = "https://esw2jqlntk.execute-api.eu-west-1.amazonaws.com/pg-dev";
+export const baseAPIUrl = "https://api.pavia.io/playground";
+// export const baseAPIUrl = "https://esw2jqlntk.execute-api.eu-west-1.amazonaws.com/pg-dev";
