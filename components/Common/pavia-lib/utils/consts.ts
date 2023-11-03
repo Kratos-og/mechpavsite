62cfa3cce7640b85cf125f1d3a1bd34f0f07a0ca
Philip <philipmarsh152@gmail.com>
2023-11-03 11:24:16 +0000

    updated storage location

--- components/Common/pavia-lib/utils/consts.ts
+++ components/Common/pavia-lib/utils/consts.ts
@@ -1,2 +1,4 @@
 // export const baseAPIUrl = "https://api.pavia.io/playground";
-export const baseAPIUrl = "https://esw2jqlntk.execute-api.eu-west-1.amazonaws.com/pg-dev";
+// export const baseAPIUrl = "https://esw2jqlntk.execute-api.eu-west-1.amazonaws.com/pg-dev";
+
+export const baseAPIUrl = process.env.NEXT_PUBLIC_PAVIA_GAME_API;
